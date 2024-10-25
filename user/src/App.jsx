4b8cee76065ebe93da69972cdc6e1057f30f50dc
Jared588 <jaredeprice@gmail.com>
2024-10-25 19:56:32 +0200

    Fix tracking bug

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "./index.css";
 
 import Posts from "./components/Posts";
@@ -8,32 +13,42 @@ import Post from "./components/Post";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
+// Custom hook to track page views
 const useAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
     // Track page views on route changes
-    ReactGA.send("page_view", {
-      page_path: location.pathname,
+    ReactGA.send({
+      hitType: "pageview",
+      page: location,
+      title: "Custom Title",
     });
   }, [location]);
 };
 
 function App() {
-  useAnalytics(); // Call the analytics hook
-
   return (
     <Router>
-      <div className="flex flex-col h-screen">
-        <Header />
-        <Routes>
-        <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/posts/:postId" element={<Post />} />
-        </Routes>
-      </div>
+      <AnalyticsWrapper />
     </Router>
   );
 }
+
+// Wrapper component for analytics tracking
+const AnalyticsWrapper = () => {
+  useAnalytics(); // Call the analytics hook
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:postId" element={<Post />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
